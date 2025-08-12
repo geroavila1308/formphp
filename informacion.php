5b0791f45f22608eaf61e66256fd4b6ac151da9f
geroavila1308 <geroavila1308@gmail.com>
2025-08-12 17:36:21 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$identificacion= $_post ['identificacion'];
+$NomApellidos= $_póst ['NomApellidos'];
+$Cargo= $_post ['Cargo'];
+$Salario= $_post ['Salario'];
+echo "La identificacion del Empleado es".identificacion;
+echo "Los nombres y apellidos del empleado son".NomApellidos;
+echo "El cargo del Empleado es".Cargo;
+echo "El salario del Empleado es".Salario;
+?>
